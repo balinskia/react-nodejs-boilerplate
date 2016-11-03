cf58c3471de0287580d512e183de16d50b9aa9fa
Bartlomiej Rutkowski <b.rutkowski94@gmail.com>
2016-11-03 18:45:31 +0100

    Added body parser to server.

--- server/server.js
+++ server/server.js
@@ -2,16 +2,19 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const database = require('./database/database');
+const bodyParser = require('body-parser');
 
 const app = express();
 database.connect(() => {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cors());
     const indexPath = path.join(__dirname, '../client/public/index.html');
     const publicPath = express.static(path.join(__dirname, '../client/public'));
     app.use('/public', publicPath);
     app.get('/', (_, res) => {
         res.sendFile(indexPath);
     });
-    app.use(cors());
 });
 
 module.exports = {
